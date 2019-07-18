bfdb6b33747302aebc4a8d3d2b3dc117c0a5bc04
CharisHadi <charishadi@gmail.com>
2019-07-18 12:40:23 -0700

    added comments

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -4,18 +4,21 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var keys = require("./pass.js")
 
+
+//Creating connection to DB
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    //Using dotenv package, hiding db password access
     password: keys.pass,
     database: 'bamazon'
 });
 
-connection.connect();
+// connection.connect();
  
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
  
-connection.end();
+// connection.end();
